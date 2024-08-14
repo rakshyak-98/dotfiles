57ee5f2558399e9d3b619a20ff284271b9a23dd4
rakshyak-98 <rakshak1998@gmail.com>
2024-08-15 00:20:28 +0530

    modify config nvim

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -9,8 +9,8 @@ vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 
 vim.opt.swapfile = false
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
