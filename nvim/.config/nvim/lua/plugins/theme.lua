fab53c2ac2c12d988efd6a14a9a91b80f099236e
rakshyak-98 <rakshak1998@gmail.com>
2024-08-18 13:55:32 +0530

    modify nvim theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,11 @@
 return {
   {
-    "rose-pine/neovim",
+    "ellisonleao/gruvbox.nvim",
     lazy = false,
-    name = "rose-pine",
+    name = "gruvbox",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "rose-pine"
+      vim.cmd.colorscheme "gruvbox"
     end
   }
 }
