bfc0a4a70b13ad8b82ed2f62379ccb20e67ca93a
rakshyak-98 <rakshak1998@gmail.com>
2024-11-18 15:27:36 +0530

    new git config entry script

--- scripts/general-setup.sh
+++ scripts/general-setup.sh
@@ -1,4 +1,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Git global conifg
+git config --global init.defaultBranch main
+git config --global core.excludesFile ~/.config/git/.gitignore
 
