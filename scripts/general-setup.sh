1133aeaf28249ddf42f6d46829cc352ce7803372
rakshyak-98 <rakshka1998@gmail.com>
2024-12-07 16:38:02 +0530

    insert command into general-setup

--- scripts/general-setup.sh
+++ scripts/general-setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+apt install curl git gh vim stow
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
