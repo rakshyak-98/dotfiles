dc9a8eb852e28124aaf3342cc16c0cdaf84e63a4
rakshyak-98 <rakshka1998@gmail.com>
2024-12-28 10:38:06 +0530

    rfactor content

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,8 +3,18 @@ vim.g.maplocalleader = " "
 vim.g.have_nerd_font = false
 vim.g.netrw_altfile = 1
 
-require "options"
-require "keymaps"
-require "commands"
-require "lazy-bootstrap"
-require "lazy-plugins"
+-- Ensure the required modules exist and are correctly configured
+local function safe_require(module)
+    local ok, err = pcall(require, module)
+    if not ok then
+        vim.api.nvim_err_writeln("Error loading module '" .. module .. "': " .. err)
+    end
+end
+
+safe_require("options")
+safe_require("keymaps")
+safe_require("commands")
+safe_require("lazy-bootstrap")
+safe_require("lazy-plugins")
+
+-- ...additional configurations if needed...
--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,7 +1,4 @@
--- @param mod n|i|v|x
--- @param cmd string
--- @param vim_cmd string
--- @param desc table
+-- Helper function to set keymaps
 local function map(mod, cmd, vim_cmd, desc)
   local opt = { noremap = true, silent = true, desc=desc }
   vim.keymap.set(mod, cmd, vim_cmd, opt)
@@ -10,31 +7,30 @@ end
 -- [[ Basic Keymaps ]]
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 map('n', '<leader>pv', '<Cmd>Explore<CR>')
-map('v', '<leader>s', ':sort ') -- yes I wnat it that way
--- vim.keymap.set('n', 'ff', format, {desc = 'Format the current buffer'}) -- fix this to enable formating
+map('v', '<leader>s', ':sort ') -- yes I want it that way
+-- vim.keymap.set('n', 'ff', format, {desc = 'Format the current buffer'}) -- fix this to enable formatting
 map('n', ']', '<Cmd>bprevious<CR>')
 map('n', '[', '<Cmd>bnext<CR>')
 map('n', '<leader>x', '<Cmd>bd<CR>')
+
+-- [[ Terminal Keymaps ]]
 map('n', '<C-t>', '<Cmd>OpenTerminal<CR>', { desc = 'Open terminal' }) -- this won't work with you
 
--- TIP: Disable arrow keys in normal mode
+-- [[ Disable Arrow Keys in Normal Mode ]]
 map('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 map('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 map('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 map('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
+-- [[ Split Navigation ]]
+-- Use CTRL+<hjkl> to switch between windows
+-- See `:help wincmd` for a list of all window commands
 map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- [[ System command ]]
+-- [[ System Command Keymaps ]]
 -- vim.api.nvim_set_keymap('n', '<M-h>', '', { noremap = true, silent = true })
-
--- ]]
 
 -- vim: ts=2 sts=2 sw=2 et
