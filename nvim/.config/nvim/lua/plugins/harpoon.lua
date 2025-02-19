e6bd92918d3ad5b6eb590afccfb99850ea732ba6
rakshyak-98 <rakshka1998@gmail.com>
2025-02-20 01:04:29 +0530

    using nvim setup of kickstart

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'ThePrimeagen/harpoon',
-	opts = function()
-		local mark = require('harpoon.mark')
-		local ui = require('harpoon.ui')
-		vim.keymap.set('n', '<leader>a', mark.add_file)
-		vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
-	end
-}
